b6d862bdba2ee6ce4bb21510483362727899ec9f
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-08 15:30:19 -0300

    Limpeza de classes

--- src/app/pages/account/sign-up-page/sign-up-page.component.ts
+++ src/app/pages/account/sign-up-page/sign-up-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
-  styleUrls: ['./sign-up-page.component.css']
 })
 export class SignUpPageComponent implements OnInit {
 
