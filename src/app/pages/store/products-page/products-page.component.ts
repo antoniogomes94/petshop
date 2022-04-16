489aaa759f9aaa4daccad2245667e94a9c31f986
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-16 19:27:25 -0300

    Criando service de integração com a API

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,6 +10,7 @@ export class ProductsPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
   }
 
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+
+    public url = 'http://localhost:3000/v1';
+
+    constructor(private http: HttpClient) { }
+
+    getProducts() {
+        return this.http.get<any[]>(`${this.url}/products`);
+    }
+}
