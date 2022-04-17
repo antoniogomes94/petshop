91e7fa070ef2c148cb82ab0d484641d5db023df2
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-16 23:21:26 -0300

    Ajustes pagina de produtos

--- src/app/pages/master/frame-page/frame-page.component.ts
+++ src/app/pages/master/frame-page/frame-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-frame-page',
   templateUrl: './frame-page.component.html',
-  styleUrls: ['./frame-page.component.css']
 })
 export class FramePageComponent implements OnInit {
 
--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent implements OnInit {
-
-  constructor() { }
+  public products$!: Observable<Product[]>;
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
-  
+    this.products$ = this.data.getProducts();
   }
 
 }
